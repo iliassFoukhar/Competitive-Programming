ae77f633892983d53cb702a6adfca96a9426dc9c
Iliass Foukhar <63895134+iliassFoukhar@users.noreply.github.com>
2022-02-09 14:47:56 +0100

    30 Days Problem Solving part 5

---
+++ codeforces/1605/A.cpp
@@ -0,0 +1,41 @@
+//DIABLOX
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define pb push_back
+#define printv(v) for(int fi = 0; fi < v.size(); fi++) cout << v[fi] << " "; cout << endl;
+#define all(a) a.begin(), a.end()
+#define min3(a,b,c) min(a,min(b,c))
+#define min4(a,b,c,d) min(a,min(b,min(c, d)))
+#define max3(a,b,c) max(a,max(b,c))
+#define REP(i, a, b) for(int i = a; i < b; i++)
+#define FOR(i, a) for(auto & i : a)
+#define len(s) (int)s.size()
+
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+// FAST INPUT
+void DIABLOX(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout << setprecision(8) << fixed;
+}
+
+// #ifndef ONLINE_JUDGE
+//     freopen("in", "r", stdin);
+//     freopen("out", "w", stdout);
+// #endif
+
+
+int main(){
+    DIABLOX();
+    ll tc = 1;
+    cin >> tc;
+    while(tc--){
+        ll a, b, c; cin >> a >> b >> c;
+        cout << (!((a + b + c) % 3) ? 0 : 1 ) << endl;
+    }
+    return 0;
+}
